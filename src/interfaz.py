906ef68e58ca539082c5e419493fa732ae7ea0ac
franciscojmr21 <fjmr.10messi@gmail.com>
2023-04-06 16:39:57 +0200

    Estructura del proyecto
    Co-authored-by: Juanca <juancaguillens01@users.noreply.github.com>

---
+++ src/interfaz.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+
+def inicializar():
+    
+    # Crear la ventana principal
+    ventana = tk.Tk()
+
+    #cambiar el logo de la ventana
+    ventana.iconbitmap()
+
+
+    ventana.title("IMDB")
+    
+
+    ventana.geometry("800x500")
+
+    # Añadir un botón a la ventana
+    boton = tk.Button(ventana, text="Haz clic aquí")
+
+    # Ubicar el botón en la ventana
+    boton.pack()
+
+    # Iniciar el loop principal de la ventana
+    ventana.mainloop()
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import interfaz as interfaz
+
+def main():
+    interfaz.inicializar()
+    
+    
+
+if __name__ == "__main__":
+    main()
+
